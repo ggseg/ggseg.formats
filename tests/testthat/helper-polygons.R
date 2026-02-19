204fc6c0d0cd1fa56b879c2e89527394bd55953b
GitHub Action <action@github.com>
2026-02-19 19:49:54 +0000

    Update coverage badge

--- tests/testthat/helper-polygons.R
+++
@@ -1,8 +0,0 @@
-make_polygon <- function(coords = c(0, 0, 1, 0, 1, 1, 0, 0)) {
-  sf::st_polygon(list(matrix(coords, ncol = 2, byrow = TRUE)))
-}
-
-make_polygon2 <- function() {
-  coords <- c(2, 0, 3, 0, 3, 1, 2, 0)
-  sf::st_polygon(list(matrix(coords, ncol = 2, byrow = TRUE)))
-}
